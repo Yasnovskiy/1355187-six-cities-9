ac17d7b35423c81d05593d0cc59f1f22ec3d28e0
Yasnovskiy <andrey5.1997@mail.ru>
2022-04-02 16:44:57 +0500

    commit(7.1.4): Работа с редусерами и действиями

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,10 +1,4 @@
 import { createAction } from '@reduxjs/toolkit';
-import { Offer } from '../types/offers';
-import { AuthorizationStatus } from '../const';
+import {AppRoute} from '../const';
 
-export const setCity = createAction<string>('main/setCity');
-
-export const setOffer = createAction<Offer[]>('main/setOffers');
-
-export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
-
+export const redirectToRoute = createAction<AppRoute>('main/redirectToRoute');
